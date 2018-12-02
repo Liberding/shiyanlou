17c875183cd90c522800b6583e1d039dcb42e5fa
liberding <lioding@msn.com>
2018-12-02 13:00:08 +0800

    多级爬取

---
+++ 17-scrapy/shiyanloudata/shiyanloudata/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ShiyanloudataItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    name = scrapy.Field()
+    update_time = scrapy.Field()
+    commites = scrapy.Field()
+    branches = scrapy.Field()
+    releases = scrapy.Field()
